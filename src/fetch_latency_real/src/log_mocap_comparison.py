dab7d42c635c8125a674fc591dfbe0b92dda92ec
ChinmayAmrutkar <chinmayamrutkar01@gmail.com>
2025-07-03 15:30:55 -0700

    Changes in log file

--- src/fetch_latency_real/src/log_mocap_comparison.py
+++ src/fetch_latency_real/src/log_mocap_comparison.py
@@ -2,87 +2,133 @@
 import rospy
 import csv
 import os
-import message_filters
-from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+import tf
+import math
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseStamped
 
 class MocapComparer:
-        def __init__(self):
-            rospy.init_node('mocap_comparer', anonymous=True)
+    def __init__(self):
+        rospy.init_node('mocap_comparer', anonymous=True)
 
-            # --- CONFIGURATION ---
-            # The name of the rigid body you created in your mocap software
-            mocap_object_name = 'Fetch1'
-            # The topic published by your mocap ROS node
-            self.mocap_topic = '/vrpn_client_node/'+mocap_object_name+'/pose'
+        # --- CONFIGURATION ---
+        mocap_object_name = 'Fetch1'
+        self.mocap_topic = '/vrpn_client_node/{}/pose'.format(mocap_object_name)
+        self.log_file_path = os.path.join(os.path.expanduser('~'), 'amcl_vs_mocap_log.csv')
+        
+        # TF frames for AMCL's estimated pose
+        self.map_frame = 'map'
+        self.base_frame = 'base_link'
 
+        # --- TF LISTENER ---
+        # This will get the continuous pose estimate from AMCL's transform
+        self.tf_listener = tf.TransformListener()
 
-            self.amcl_topic = '/amcl_pose'
-            # Output file path
-            self.log_file_path = os.path.join(os.path.expanduser('~'), 'amcl_vs_mocap_log.csv')
+        # --- DATA STORAGE ---
+        self.latest_mocap_pose = None
+        self.initial_amcl_pose = None
+        self.initial_mocap_pose = None
 
-            # --- SUBSCRIBERS ---
-            rospy.loginfo("Subscribing to AMCL on: " + self.amcl_topic)
-            amcl_sub = message_filters.Subscriber(self.amcl_topic, PoseWithCovarianceStamped)
-            
-            rospy.loginfo("Subscribing to Mocap on:" + self.mocap_topic)
-            mocap_sub = message_filters.Subscriber(self.mocap_topic, PoseStamped)
+        # --- SUBSCRIBER for Mocap Ground Truth ---
+        rospy.Subscriber(self.mocap_topic, PoseStamped, self.mocap_callback)
+        rospy.loginfo("Subscribing to Mocap on: %s", self.mocap_topic)
 
-            # --- SYNCHRONIZER ---
-            # Synchronize messages by timestamp
-            self.ts = message_filters.ApproximateTimeSynchronizer([amcl_sub, mocap_sub], queue_size=10, slop=0.1)
-            self.ts.registerCallback(self.data_callback)
+        # --- CSV SETUP ---
+        self.csv_file = open(self.log_file_path, 'w')
+        self.csv_writer = csv.writer(self.csv_file)
+        # Added yaw columns for heading
+        self.csv_writer.writerow(['timestamp', 'amcl_x', 'amcl_y', 'amcl_yaw', 'mocap_x', 'mocap_y', 'mocap_yaw', 'error_x', 'error_y', 'error_yaw', 'error_distance'])
+        rospy.loginfo("Logging comparison data to: %s", self.log_file_path)
 
-            # --- CSV SETUP ---
-            self.csv_file = open(self.log_file_path, 'w')
-            self.csv_writer = csv.writer(self.csv_file)
-            self.csv_writer.writerow(['timestamp', 'amcl_x', 'amcl_y', 'mocap_x', 'mocap_y', 'error_x', 'error_y', 'error_distance'])
-            rospy.loginfo("Logging comparison data to:" + self.log_file_path)
-            self.init_amcl =None
-            self.init_mocap = None
+        # --- MAIN PROCESSING LOOP ---
+        rospy.Timer(rospy.Duration(0.5), self.processing_loop)
 
-        def data_callback(self, amcl_msg, mocap_msg):
-            amcl_pose = amcl_msg.pose.pose
-            mocap_pose = mocap_msg.pose
-            if self.init_amcl is None:
-                        self.init_amcl = amcl_pose 
-            if self.init_mocap is None:
-                self.init_mocap = mocap_pose
-		
+    def mocap_callback(self, msg):
+        """This function is called every time a new Mocap message arrives."""
+        self.latest_mocap_pose = msg.pose
+        # If this is the first message, store it as the initial pose
+        if self.initial_mocap_pose is None:
+            self.initial_mocap_pose = self.latest_mocap_pose
+            rospy.loginfo("Captured initial Mocap pose.")
 
-            if self.init_mocap is None or self.init_amcl is None:
-                        rospy.loginfo("Nonetype init pose {},{}:".format(( self.init_mocap is None),( self.init_amcl is None)))
-                        return None
+    def processing_loop(self, event):
+        """This is our main loop, running at a fixed rate."""
+        try:
+            # Get the continuous AMCL pose estimate using the TF transform
+            (trans, rot) = self.tf_listener.lookupTransform(self.map_frame, self.base_frame, rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn_throttle(2.0, "TF Exception: Could not get transform from '%s' to '%s'", self.map_frame, self.base_frame)
+            return
 
-            dx_amcl= self.init_amcl.position.x
-            dx_mocap = self.init_mocap.position.x
+        # Wait until we have received the first message from Mocap
+        if self.latest_mocap_pose is None:
+            rospy.loginfo_once("Waiting for initial pose from Mocap...")
+            return
 
-            dy_amcl= self.init_amcl.position.y
-            dy_mocap = self.init_mocap.position.y
+        # Capture the initial AMCL pose from the first successful TF lookup
+        if self.initial_amcl_pose is None:
+            self.initial_amcl_pose = {'trans': trans, 'rot': rot}
+            rospy.loginfo("Captured initial AMCL pose from TF.")
+            return # Skip the first loop to ensure all initial poses are set
 
-            # Calculate error
-            error_x = (amcl_pose.position.x-dx_amcl) - (mocap_pose.position.x-dx_mocap)
-            error_y = (amcl_pose.position.y-dy_amcl) - (mocap_pose.position.y-dy_mocap)
-            error_dist = (error_x**2 + error_y**2)**0.5
-	    rospy.loginfo("AMCL [{},{}] MOCAP [{},{}] ERR [{},{}]".format(
-				round(amcl_pose.position.x,2),round(amcl_pose.position.y,2),
-				round(mocap_pose.position.x,2),round(mocap_pose.position.y,2),
-				round(error_x,2),round(error_y,2)
-			))
-            # Write to CSV
-            self.csv_writer.writerow([
-                amcl_msg.header.stamp.to_sec(),
-                amcl_pose.position.x,
-                amcl_pose.position.y,
-                mocap_pose.position.x,
-                mocap_pose.position.y,
-                error_x,
-                error_y,
-                error_dist
-            ])
+        # --- Extract current poses and headings ---
+        amcl_x, amcl_y, amcl_z = trans
+        mocap_x = self.latest_mocap_pose.position.x
+        mocap_y = self.latest_mocap_pose.position.y
+        
+        # Convert quaternions to yaw (heading)
+        _, _, amcl_yaw = euler_from_quaternion(rot)
+        _, _, mocap_yaw = euler_from_quaternion([
+            self.latest_mocap_pose.orientation.x,
+            self.latest_mocap_pose.orientation.y,
+            self.latest_mocap_pose.orientation.z,
+            self.latest_mocap_pose.orientation.w
+        ])
 
-        def run(self):
-            rospy.on_shutdown(lambda: self.csv_file.close())
-            rospy.spin()
+        # --- Calculate relative displacement and error ---
+        init_amcl_x, init_amcl_y, _ = self.initial_amcl_pose['trans']
+        init_mocap_x = self.initial_mocap_pose.position.x
+        init_mocap_y = self.initial_mocap_pose.position.y
+
+        error_x = (amcl_x - init_amcl_x) - (mocap_x - init_mocap_x)
+        error_y = (amcl_y - init_amcl_y) - (mocap_y - init_mocap_y)
+        error_dist = math.sqrt(error_x**2 + error_y**2)
+
+        # Calculate heading error, handling angle wrapping
+        _, _, initial_amcl_yaw = euler_from_quaternion(self.initial_amcl_pose['rot'])
+        _, _, initial_mocap_yaw = euler_from_quaternion([
+            self.initial_mocap_pose.orientation.x,
+            self.initial_mocap_pose.orientation.y,
+            self.initial_mocap_pose.orientation.z,
+            self.initial_mocap_pose.orientation.w
+        ])
+        
+        amcl_delta_yaw = amcl_yaw - initial_amcl_yaw
+        mocap_delta_yaw = mocap_yaw - initial_mocap_yaw
+        error_yaw = math.atan2(math.sin(amcl_delta_yaw - mocap_delta_yaw), math.cos(amcl_delta_yaw - mocap_delta_yaw))
+
+        rospy.loginfo("ERR [Dist:%.3f m, Yaw:%.3f rad]", error_dist, error_yaw)
+
+        # --- Write to CSV ---
+        self.csv_writer.writerow([
+            rospy.Time.now().to_sec(),
+            amcl_x,
+            amcl_y,
+            amcl_yaw,
+            mocap_x,
+            mocap_y,
+            mocap_yaw,
+            error_x,
+            error_y,
+            error_yaw,
+            error_dist
+        ])
+
+    def run(self):
+        rospy.on_shutdown(lambda: self.csv_file.close())
+        rospy.spin() # Keeps the node alive
+
 if __name__ == '__main__':
-        comparer = MocapComparer()
-        comparer.run()
+    comparer = MocapComparer()
+    comparer.run()
+
