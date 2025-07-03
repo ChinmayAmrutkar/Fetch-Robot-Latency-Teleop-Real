40cdfa24f5aa74024f14a7e0e2b9b7d6633e7d59
ChinmayAmrutkar <chinmayamrutkar01@gmail.com>
2025-07-03 12:38:11 -0700

    Adding MOKAP logger file

---
+++ src/fetch_latency_real/src/log_mocap_comparison.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import rospy
+import csv
+import os
+import message_filters
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+
+class MocapComparer:
+        def __init__(self):
+            rospy.init_node('mocap_comparer', anonymous=True)
+
+            # --- CONFIGURATION ---
+            # The name of the rigid body you created in your mocap software
+            mocap_object_name = 'fetch'
+            # The topic published by your mocap ROS node
+            self.mocap_topic = f'/vrpn_client_node/{mocap_object_name}/pose'
+            self.amcl_topic = '/amcl_pose'
+            # Output file path
+            self.log_file_path = os.path.join(os.path.expanduser('~'), 'amcl_vs_mocap_log.csv')
+
+            # --- SUBSCRIBERS ---
+            rospy.loginfo(f"Subscribing to AMCL on: {self.amcl_topic}")
+            amcl_sub = message_filters.Subscriber(self.amcl_topic, PoseWithCovarianceStamped)
+            
+            rospy.loginfo(f"Subscribing to Mocap on: {self.mocap_topic}")
+            mocap_sub = message_filters.Subscriber(self.mocap_topic, PoseStamped)
+
+            # --- SYNCHRONIZER ---
+            # Synchronize messages by timestamp
+            self.ts = message_filters.ApproximateTimeSynchronizer([amcl_sub, mocap_sub], queue_size=10, slop=0.1)
+            self.ts.registerCallback(self.data_callback)
+
+            # --- CSV SETUP ---
+            self.csv_file = open(self.log_file_path, 'w')
+            self.csv_writer = csv.writer(self.csv_file)
+            self.csv_writer.writerow(['timestamp', 'amcl_x', 'amcl_y', 'mocap_x', 'mocap_y', 'error_x', 'error_y', 'error_distance'])
+            rospy.loginfo(f"Logging comparison data to: {self.log_file_path}")
+
+        def data_callback(self, amcl_msg, mocap_msg):
+            amcl_pose = amcl_msg.pose.pose
+            mocap_pose = mocap_msg.pose
+
+            # Calculate error
+            error_x = amcl_pose.position.x - mocap_pose.position.x
+            error_y = amcl_pose.position.y - mocap_pose.position.y
+            error_dist = (error_x**2 + error_y**2)**0.5
+
+            # Write to CSV
+            self.csv_writer.writerow([
+                amcl_msg.header.stamp.to_sec(),
+                amcl_pose.position.x,
+                amcl_pose.position.y,
+                mocap_pose.position.x,
+                mocap_pose.position.y,
+                error_x,
+                error_y,
+                error_dist
+            ])
+
+        def run(self):
+            rospy.on_shutdown(lambda: self.csv_file.close())
+            rospy.spin()
+if __name__ == '__main__':
+        comparer = MocapComparer()
+        comparer.run()
